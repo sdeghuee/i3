79a4b681f40c83a161c92bbd9b1ca709a2abfca3
sdeghuee <sdeghuee@purdue.edu>
2016-12-06 12:40:40 -0500

    comments for startup file

---
+++ startup.sh
@@ -0,0 +1,7 @@
+setxkbmap -option caps:super 		#sets caps lock as mod key
+nm-applet & 				#should start network manager pull down in i3status
+pulseaudio --start & 			#for audio control, starts auido
+syndaemon -i 0.5 -d 			#sets a delay between typing and moving the trackpad
+xinput set-prop 13 "Device Enabled" 0 	#disables touchpad, change 13 to number from xinput
+feh --bg-scale ~/.wallpaper/* 		#put whatever image you want into a folder named .wallpaper
+#xinput set-prop 13 "Evdev Wheel Emulation Button" 2 #setting for changing type of middle click
