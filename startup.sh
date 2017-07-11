87dd9fde65b9b136ee5ef4be52d926105c03afad
Sam Deghuee <sdeghuee@gmail.com>
2017-07-11 16:47:27 -0400

    i3 configuration for t440p

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ pulseaudio --start & 			#for audio control, starts auido
 syndaemon -i 0.5 -d 			#sets a delay between typing and moving the trackpad
 #xinput set-prop 13 "Device Enabled" 0 	#disables touchpad, change 13 to number from xinput
 feh --bg-scale ~/.wallpaper/* 		#put whatever image you want into a folder named .wallpaper
-#xinput set-prop 13 "Evdev Wheel Emulation Button" 2 #setting for changing type of middle click
+#xinput set-prop 14 "Evdev Wheel Emulation Button" 2 #setting for changing type of middle click 13 for x220 14 for t440p
