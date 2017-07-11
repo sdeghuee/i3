5e00b8b248e4e16686fa4f963dfd7ad6431af6db
sdeghuee <sdeghuee@purdue.edu>
2017-07-11 16:49:51 -0400

    created branch for x220t

--- startup.sh
+++ startup.sh
@@ -5,3 +5,4 @@ syndaemon -i 0.5 -d 			#sets a delay between typing and moving the trackpad
 #xinput set-prop 13 "Device Enabled" 0 	#disables touchpad, change 13 to number from xinput
 feh --bg-scale ~/.wallpaper/* 		#put whatever image you want into a folder named .wallpaper
 #xinput set-prop 13 "Evdev Wheel Emulation Button" 2 #setting for changing type of middle click
+google-drive-ocamlfuse ~/drive/ #mounts google drive to the drive folder home
