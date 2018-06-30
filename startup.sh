cd49e22ad5732fd0ed99b18e087043a88faea2b4
sdeghuee <sdeghuee@gmail.com>
2018-06-30 12:55:10 -0400

    Updated to fix nautilus desktop issue. Additional config changes

--- startup.sh
+++ startup.sh
@@ -3,5 +3,7 @@ nm-applet & 				#should start network manager pull down in i3status
 pulseaudio --start & 			#for audio control, starts auido
 syndaemon -i 0.5 -d 			#sets a delay between typing and moving the trackpad
 #xinput set-prop 13 "Device Enabled" 0 	#disables touchpad, change 13 to number from xinput
-feh --bg-scale ~/.wallpaper/* 		#put whatever image you want into a folder named .wallpaper
+feh --bg-scale --randomize ~/.wallpaper/* 		#put whatever image you want into a folder named .wallpaper
 #xinput set-prop 13 "Evdev Wheel Emulation Button" 2 #setting for changing type of middle click
+compton -b
+gsettings set org.gnome.desktop.background show-desktop-icons false
